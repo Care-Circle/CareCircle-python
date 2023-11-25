f6a236dbc8d2a5f066fc32412bf5f044c6454c8f
Mari Spinola <maspinola123@gmail.com>
2023-11-25 19:30:30 -0300

    fix: identation

--- Menu.py
+++ Menu.py
@@ -1,10 +1,25 @@
 resp = 1
 
-suporte_comunitario_info = ["Suporte Comunitário", "O suporte Comunitário é um local onde o usuário poderá compartilhar experiências, fazer perguntas e oferecer apoio uns aos outros."]
-diario_saude_info = ["Diário de Saúde Personalizado", "Diário de Saúde Personalizado é uma funcionalidade que permite que usuários mantenham um diário de saúde pessoal, onde eles possam registrar sintomas, emoções, atividades físicas e outros detalhes relevantes para o seu bem-estar."]
-registro_menstrual_info = ["Registro Menstrual", "A funcionalidade de Registro Menstrual permite que usuários rastreiem seu ciclo menstrual, sintomas associados e humor."]
-integracao_dispositivos_info = ["Integração de Dispositivos de Saúde", "A funcionalidade de Integração de Dispositivos de Saúde permite que os usuários façam a integração de dispositivos de saúde, como smartwatches e monitores de pressão arterial, para coletar dados de saúde em tempo real."]
-acompanhamento_transicao_info = ["Acompanhamento da Transição de Gênero", "A funcionalidade de Acompanhamento da Transição de Gênero permite que usuários registrem e acompanhem marcos na transição de gênero, como hormonioterapia, cirurgias e consultas médicas especializadas."]
+suporte_comunitario_info = [
+    "Suporte Comunitário",
+    "O suporte Comunitário é um local onde o usuário poderá compartilhar experiências, fazer perguntas e oferecer apoio uns aos outros.",
+]
+diario_saude_info = [
+    "Diário de Saúde Personalizado",
+    "Diário de Saúde Personalizado é uma funcionalidade que permite que usuários mantenham um diário de saúde pessoal, onde eles possam registrar sintomas, emoções, atividades físicas e outros detalhes relevantes para o seu bem-estar.",
+]
+registro_menstrual_info = [
+    "Registro Menstrual",
+    "A funcionalidade de Registro Menstrual permite que usuários rastreiem seu ciclo menstrual, sintomas associados e humor.",
+]
+integracao_dispositivos_info = [
+    "Integração de Dispositivos de Saúde",
+    "A funcionalidade de Integração de Dispositivos de Saúde permite que os usuários façam a integração de dispositivos de saúde, como smartwatches e monitores de pressão arterial, para coletar dados de saúde em tempo real.",
+]
+acompanhamento_transicao_info = [
+    "Acompanhamento da Transição de Gênero",
+    "A funcionalidade de Acompanhamento da Transição de Gênero permite que usuários registrem e acompanhem marcos na transição de gênero, como hormonioterapia, cirurgias e consultas médicas especializadas.",
+]
 
 while resp != 0:
     print("1 - Suporte Comunitário")
